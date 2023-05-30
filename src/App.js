6ad7ee5256b3174f046ff511a8a824a5ff5f8fdd
Frank Rubino <rubinof+github@mac.com>
2023-05-30 15:19:42 -0400

    Added baby to the message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React in less than a day
+          Learn React in less than a day, baby
         </a>
       </header>
     </div>
