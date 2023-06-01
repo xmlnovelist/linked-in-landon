29d0fb0bfedac8e614b009d621d98b40ae544705
Frank Rubino <rubinof+github@mac.com>
2023-05-31 22:31:08 -0400

    Components created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Footer from './components_orig/Footer.js'
+import Header from './components_orig/Header.js'
+import Main from './components_orig/Main.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React in less than a day, baby
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() { 
+    return (
+      <div className="App">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
